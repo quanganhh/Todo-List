d6370df9fa80d875bb99ee3ad11f1d0ca687139d
Quang Anh <nguyen.quang.anh@sun-asterisk.com>
2019-05-30 15:56:22 +0700

    Todo List

---
+++ build/precache-manifest.e61c33d8880a35906b33fac931b121ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b120015ecf74a32a83888a749ec9335f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad72e0623559f8590eec",
+    "url": "/static/css/main.34239def.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "ad72e0623559f8590eec",
+    "url": "/static/js/main.2174d8e9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
